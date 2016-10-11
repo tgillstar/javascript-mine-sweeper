d734d8fbab1298248e6a0f8a842c5322d0eaf7e1
Tiffany Gill <tiffany_gill@kenan-flagler.unc.edu>
2016-10-11 17:03:30 -0400

    Further reworking of Board object and grooming of Tests
    Pulled out another reusable piece of code to make the
    setElementAttributes function. Changed name of loopThroughGameBoard
    function to make it sound more reusable. Tweaked unit test file to
    reflect these changes as well as a bit of grooming for further tests to
    come.

--- js/mSweeper.js
+++ js/mSweeper.js
@@ -20,13 +20,20 @@
     ];
 
     Board.prototype = {
-
-        loopThroughGameBoard: function (performThisWork){
+        forLoopThroughItems: function (performThisWork){
+            var self = this;
             for (var i = 0; i < this.cols; i++) {
                 for (var j = 0; j < this.rows; j++) {
-                    performThisWork(i,j);
+                    performThisWork(i, j, self);
                 }
             }
+        },
+
+        setElementAttributes: function (element, attributesArr) {
+            for (var item in attributesArr) {
+                element.setAttribute(attributesArr[item].name, attributesArr[item].value)
+            }
+            return element;
         },
 
         createGameBoard: function () {
@@ -36,13 +43,15 @@
                     var piecePlacement = document.createElement("li"),
                     gamePiece = document.createElement("button"),
                     gamePieceText = document.createTextNode("?"),
-                    liName = 'id_' + i.toString() + j.toString(),
+                    gamePieceName = 'id_' + i.toString() + j.toString(),
                     btnName = 'btn_' + i.toString() + j.toString();
-
                     gamePiece.appendChild(gamePieceText);
-                    gamePiece.setAttribute("id", btnName);
-                    gamePiece.setAttribute("class", "questionBtn");
-                    piecePlacement.setAttribute("id", liName);
+
+                    var gamePieceAttrs = [{name: 'id', value: btnName}, {name: 'class', value: 'questionBtn'}];
+                    gamePiece = this.setElementAttributes(gamePiece, gamePieceAttrs);
+
+                    var piecePlacementAttrs = [{name: 'id', value: gamePieceName}];
+                    piecePlacement = this.setElementAttributes(piecePlacement, piecePlacementAttrs);
                     piecePlacement.appendChild(gamePiece);
                     gameBoard.appendChild(piecePlacement);
                 }
@@ -52,16 +61,15 @@
             document.getElementById("boardgame").appendChild(gameBoard);
         },
 
-        addValuesToGamePieces: function (i,j) {
+        addValuesToGamePieces: function (i, j, self) {
             var id = i.toString() + j.toString(),
                 btnName = 'btn_' + id,
                 getPiece = document.getElementById(btnName);
-            
             getPiece.value = boardValues[i][j].toString();
         },
 
-        showValueOfButton: function (liName, btnValue) {
-            document.getElementById(liName).innerHTML = "<span>"+ btnValue+ "</span>";
+        showValueOfButton: function (gamePieceName, btnValue) {
+            document.getElementById(gamePieceName).innerHTML = "<span>"+ btnValue+ "</span>";
         },
 
         showValueOfButtonDelegate: function (outter, inner) {
@@ -71,17 +79,12 @@
             }
         },
 
-        addClickEventToGamePieces: function () {
-            var self = this;
-            for (var i = 0; i < this.cols; i++) {
-                for (var j = 0; j < this.rows; j++) {
-                    var id = i.toString() + j.toString();
-                    var liName = 'id_' + id;
-                    var btnName = 'btn_' + id;
-                    var elem = document.getElementById(btnName);
-                    elem.addEventListener("click", self.showValueOfButtonDelegate(liName, elem.value),false);
-                }
-            }
+        addClickEventToGamePieces: function (i, j, self) {
+            var id = i.toString() + j.toString(),
+                gamePieceName = 'id_' + id,
+                btnName = 'btn_' + id,
+                elem = document.getElementById(btnName);
+            elem.addEventListener("click", self.showValueOfButtonDelegate(gamePieceName, elem.value),false);
         }
     };
 
--- test/mSweeper.test.js
+++ test/mSweeper.test.js
@@ -24,123 +24,72 @@ describe("Mine Sweeper", function() {
         });
     });
 
-    describe("#loopThroughGameBoard", function() {
-        beforeEach(function() {
-            //Will run before all tests in this block
-            this.board = new Board();
-        });
+    describe("Helper functions:", function() {
+        describe("#forLoopThroughItems", function(){
+            beforeEach(function() {
+                //Will run before all tests in this block
+                this.board = new Board();
+            });
 
-        it("boardgame div element is in the DOM", function () {
-            expect(document.getElementById("boardgame")).to.not.be.null;
-        });
+            it("boardgame div element is in the DOM", function () {
+                expect(document.getElementById("boardgame")).to.not.be.null;
+            });
 
-        it("should loop through basic gameboard", function() {
-            function printMe (i, j) {
-                console.log(i,j);
-            }
-            this.board.loopThroughGameBoard(printMe);
+            it("should loop through basic gameboard", function() {
+                function printMe (i, j) {
+                    console.log(i,j);
+                }
+                this.board.forLoopThroughItems(printMe);
+            });
         });
     });
 
-    describe("#createGameBoard", function() {
-        beforeEach(function() {
-            //Will run before all tests in this block
-            this.board = new Board();
-            this.board.createGameBoard();
-        });
-        afterEach(function() {
-            //Will run after all tests in this block
-            var myNode = document.getElementById("boardgame");
-            while (myNode.firstChild) {
-                myNode.removeChild(myNode.firstChild);
-            }
+    describe("Game board Mocked", function() {
+        describe("#createGameBoard", function() {
+            beforeEach(function() {
+                //Will run before all tests in this block
+                this.board = new Board();
+                this.board.createGameBoard();
+            });
+            afterEach(function() {
+                //Will run after all tests in this block
+                var myNode = document.getElementById("boardgame");
+                while (myNode.firstChild) {
+                    myNode.removeChild(myNode.firstChild);
+                }
+            });
+
+            it("first placeholder (li) to be on the board", function () {
+                expect(document.getElementById("id_00")).to.not.be.null;
+            });
+
+            it("first game piece (? button) to be on the board", function () {
+                expect(document.getElementById("btn_00")).to.not.be.null;
+            });
         });
 
-        it("first placeholder (li) to be on the board", function () {
-            expect(document.getElementById("id_00")).to.not.be.null;
-        });
+        describe("#addValuesToGamePieces", function() {
+            beforeEach(function() {
+                //Will run before all tests in this block
+                this.board = new Board();
+                this.board.createGameBoard();
+                this.board.forLoopThroughItems(this.board.addValuesToGamePieces);
+            });
+            afterEach(function() {
+                //Will run after all tests in this block
+                var myNode = document.getElementById("boardgame");
+                while (myNode.firstChild) {
+                    myNode.removeChild(myNode.firstChild);
+                }
+            });
 
-        it("first game piece (? button) to be on the board", function () {
-            expect(document.getElementById("btn_00")).to.not.be.null;
-        });
-    });
+            it("game piece (btn_00) value should be 0 on default 8x8 board", function () {
+                document.getElementById("btn_00").value.should.equal('0');
+            });
 
-    describe("#addValuesToGamePieces", function() {
-        beforeEach(function() {
-            //Will run before all tests in this block
-            this.board = new Board();
-            this.board.createGameBoard();
-            this.board.loopThroughGameBoard(this.board.addValuesToGamePieces);
-        });
-        afterEach(function() {
-            //Will run after all tests in this block
-            var myNode = document.getElementById("boardgame");
-            while (myNode.firstChild) {
-                myNode.removeChild(myNode.firstChild);
-            }
-        });
-
-        it("game piece (btn_00) value should be 0 on default 8x8 board", function () {
-            expect(document.getElementById("btn_00").value).to.equal('0');
-        });
-
-        it("game piece (btn_33) value should be 1 on default 8x8 board", function () {
-            expect(document.getElementById("btn_33").value).to.equal('1');
+            it("game piece (btn_33) value should be 1 on default 8x8 board", function () {
+                document.getElementById("btn_33").value.should.equal('1');
+            });
         });
     });
 });
-/*
-describe("Cow", function() {
-  describe("constructor", function() {
-    it("should have a default name", function() {
-      var cow = new Cow();
-      expect(cow.name).to.equal("Anon cow");
-    });
-
-    it("should set cow's name if provided", function() {
-      var cow = new Cow("Kate");
-      expect(cow.name).to.equal("Kate");
-    });
-  });
-
-  describe("#greets", function() {
-    it("should throw if no target is passed in", function() {
-      expect(function() {
-        (new Cow()).greets();
-      }).to.throw(Error);
-    });
-
-    it("should greet passed target", function() {
-      var greetings = (new Cow("Kate")).greets("Baby");
-      expect(greetings).to.equal("Kate greets Baby");
-    });
-  });
-});*/
-
-/*
- var expect = chai.expect;
-
- describe('recursion-divs', function() {
- // create html string that will be inserted into DOM
- var html = '<div></div><div></div><div></div>';
-
- // append html to body node
- $(document.body).append(html);
-
- // run my original test
- it('should return false when there are fewer than 5 divs', function() {
- var result = containsFiveOrMoreDivs(document.body);
- expect(result).to.equal(false);
- });
-
- // create a clone of the DOM, and append more divs to it
- var $domClone = $(document.body).clone();
- $domClone.append(html);
-
- // test this new DOM
- it('should return true when there are more than 5 divs', function() {
- // jQuery returns an array of DOM nodes, so use 0 index
- expect(containsFiveOrMoreDivs($domClone[0])).to.equal(true);
- });
- });
-*/
